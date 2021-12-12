ca3404e89a3d865a15e05c48020aecc1a7fb160b
Alex Dickson <alex.m.dickson@gmail.com>
2021-12-12 13:12:07 -0500

    Updated comments

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
-/* Set scores to 0 before game starts */
+// Set scores to 0 before game starts //
 let computerScore = 0;
 let playerScore = 0;
 
-/* Function for computer choice: set var choice as array of choices, set var randomChoice as random choice over length of that array. Return the choice and set to lowercase.*/
+// Function for computer choice: set var choice as array of choices, set var randomChoice as random choice over length of that array. Return the choice and set to lowercase.//
 function computerPlay() {
     const choice = ["Rock", "Paper", "Scissors"]
     let randomChoice = choice[Math.floor(Math.random() * choice.length)]
     return randomChoice.toLowerCase();
 }
 
-/* Function for round: set var computerSelection to above function, set var playerSelection to prompt (and convert to lowercase). Switch statement comparing each possible choice, including a tie. Increment playerScore or computerScore accordingly. If either score is at least 5, provide alert of winner. */
+// Function for round: set var computerSelection to above function, set var playerSelection to prompt (and convert to lowercase). Switch statement comparing each possible choice, including a tie. Increment playerScore or computerScore accordingly. If either score is at least 5, provide alert of winner. //
 function playRound() {
     let computerSelection = computerPlay();
     let playerSelection = prompt("Make your choice.").toLowerCase();
@@ -51,11 +51,11 @@ function playRound() {
     }
 }
 
-/* Function for game: loop playRound until playerScore or computerScore reaches 5. */
+// Function for game: loop playRound until playerScore or computerScore reaches 5. //
 function game() {
     for (i = 1; playerScore < 5 & computerScore < 5; i++) {
         playRound();
     }
 }
-/* Play game */
+// Play game //
 game();
